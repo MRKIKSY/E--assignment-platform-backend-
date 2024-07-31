4b4ef81e06e37d1d657944742636184fca14c0f4
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-07-31 09:33:31 +0100

    now

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import path from 'path';
 import './db.js';
 import { AdminRouter } from './routes/auth.js';
 import { studentRouter } from './routes/student.js';
@@ -11,26 +12,22 @@ import { Book } from './models/Book.js';
 import { Student } from './models/Student.js';
 import { Admin } from './models/Admin.js';
 
-dotenv.config();
+// Set up __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-
-// Middleware to parse JSON
 app.use(express.json());
-
-// Enable CORS
 app.use(cors({
     origin: ['http://localhost:5173', 'https://e-assignment-platform.onrender.com'],
     credentials: true
 }));
-
-// Cookie parser middleware
 app.use(cookieParser());
+dotenv.config();
 
-// Serve static files from the "public" directory
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
 app.use('/auth', AdminRouter);
 app.use('/student', studentRouter);
 app.use('/book', bookRouter);
@@ -40,14 +37,12 @@ app.get('/dashboard', async (req, res) => {
         const student = await Student.countDocuments();
         const admin = await Admin.countDocuments();
         const book = await Book.countDocuments();
-        return res.json({ok: true, student, book, admin});
-    } catch(err) {
+        return res.json({ ok: true, student, book, admin });
+    } catch (err) {
         return res.json(err);
     }
 });
 
-// Start the server
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log("Server is Running");
 });
