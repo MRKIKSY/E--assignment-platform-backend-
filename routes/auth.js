4239a79f16ebd03800abe831552bac8ae388930f
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-08-01 03:43:18 +0100

    nownownow

--- routes/auth.js
+++ routes/auth.js
@@ -22,12 +22,7 @@ router.post('/login', async (req, res) => {
       }
       const token = jwt.sign({ username: admin.username, role: 'admin' }, process.env.Admin_Key, { expiresIn: '1h' });
       
-      res.cookie('token', token, { 
-        httpOnly: true, 
-        secure: true, // Set to true if you're serving over HTTPS
-        sameSite: 'None' 
-      });
-  
+      
       return res.json({ login: true, role: 'admin', token });
     } else if (role === 'student') {
       const student = await Student.findOne({ username });
